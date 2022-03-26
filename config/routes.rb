163e7e6bbad747f29fb7cbb998322efb3abf9440
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-26 15:52:00 -0300

    disposition inline

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  controller :pages do
+    get :qr_code_generator
+    get :qr_code_download
+  end
 end
